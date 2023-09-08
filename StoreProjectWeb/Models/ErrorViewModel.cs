4994a0f4fb70bcd251b59f94b581185e2127d686
Emilio Ibarra <emilio.ibarra.270@gmail.com>
2023-09-08 16:42:49 -0600

    Agregar archivos de proyecto.

---
+++ StoreProjectWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace StoreProjectWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
